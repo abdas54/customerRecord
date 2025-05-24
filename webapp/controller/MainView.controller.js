3a331480c1fa2181aaa7f49fb13017fa5a8f5fe7
Abhimanyu <abhi12243@gmail.com>
2025-05-24 23:42:54 +0530

    Issue fix

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -6,93 +6,169 @@ sap.ui.define([
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,JSONModel,MessageBox) {
+    function (Controller, JSONModel, MessageBox) {
         "use strict";
 
         return Controller.extend("com.eros.customerrecord.controller.MainView", {
             onInit: function () {
                 this.oModel = this.getOwnerComponent().getModel();
+                this.oModel.setSizeLimit(1000);
                 this.custModel = new JSONModel();
-                this.getView().setModel(this.custModel,"custModel");
+                this.getView().setModel(this.custModel, "custModel");
             },
-            onAddAddress: function(){
+            onAddAddress: function () {
 
-                if(!this.getView().byId("workSection").getVisible()){
+                if (!this.getView().byId("workSection").getVisible()) {
                     this.getView().byId("workSection").setVisible(true);
                 }
-                else if(!this.getView().byId("otherSection").getVisible()){
+                else if (!this.getView().byId("otherSection").getVisible()) {
                     this.getView().byId("otherSection").setVisible(true);
                 }
             },
-            onCustomerTypeChange: function(oEvent){
+            onCustomerTypeChange: function (oEvent) {
 
-                if(oEvent.getParameter("selectedItem").getProperty("key") === "2"){
+                if (oEvent.getParameter("selectedItem").getProperty("key") === "TOURIST") {
                     this.getView().byId("cardTypelbl").setRequired(true);
                     this.getView().byId("issuedBylbl").setRequired(true);
                     this.getView().byId("cardNumberlbl").setRequired(true);
+                    this.getView().byId("cardExpirylbl").setRequired(true);
                 }
-                else{
-                this.getView().byId("cardTypelbl").setRequired(false);
-                this.getView().byId("issuedBylbl").setRequired(false);
-                this.getView().byId("cardNumberlbl").setRequired(false);
+                else {
+                    this.getView().byId("cardTypelbl").setRequired(false);
+                    this.getView().byId("issuedBylbl").setRequired(false);
+                    this.getView().byId("cardNumberlbl").setRequired(false);
+                    this.getView().byId("cardExpirylbl").setRequired(false);
                 }
 
             },
-            onSearchNumber: function(oEvent){
-                var that=this;
+            onSearchNumber: function (oEvent) {
+                var that = this;
                 var searchNumber = oEvent.getParameter("query");
-                this.oModel.read("/ZER_CUST_MASTERSet(Kunnr='',MobNo='" + searchNumber + "')", {
-                    success: function(oData) {
-                        if(oData){
-                            
-                            that.getView().getModel("custModel").setData({});
-                            that.getView().getModel("custModel").setData(oData);
-                            if((oData.home_add1 !== "") || (oData.home_add2 !== "")){
-                                that.getView().byId("addHomeType").setSelectedKey("1");
-                            }
-                            if((oData.off_add1 !== "") || (oData.off_add2 !== "")){
-                                that.getView().byId("addWorkType").setSelectedKey("2");
-                                that.getView().byId("workSection").setVisible(true);
-                            }
-                            if((oData.oth_Add1 !== "") || (oData.oth_Add2 !== "")){
-                                that.getView().byId("addOtherType").setSelectedKey("3");
-                                that.getView().byId("otherSection").setVisible(true);
-                            }
-                            that.getView().getModel("custModel").refresh();
-                            
-                            
-                        }
-                        
-                    },
-                    error: function(oError) {
-                        sap.m.MessageBox.show("Customer does not exist. Kindly add it",{
-                            icon: sap.m.MessageBox.Icon.Error,
-                            title: "Error",
-                            actions: [MessageBox.Action.OK],
-                            onClose: function (oAction) {
-                                if (oAction === MessageBox.Action.OK) {
+                var aFilters = [];
+
+                aFilters.push(new sap.ui.model.Filter("Mobile", sap.ui.model.FilterOperator.EQ, searchNumber));
+                this.oModel.read("/CustomerSet",
+                    {
+                        filters: aFilters,
+                        success: function (oData) {
+                            if (oData) {
+                                if (oData.results.length > 0) {
                                     that.getView().getModel("custModel").setData({});
+                                    that.getView().getModel("custModel").setData(oData.results[0]);
+                                    if ((oData.results[0].HomeAddressLine1 !== "") || (oData.results[0].HomeAddressLine2 !== "")) {
+                                        that.getView().byId("addHomeType").setSelectedKey("1");
+                                    }
+                                    if ((oData.results[0].OfficeAddressLine1 !== "") || (oData.results[0].OfficeAddressLine2 !== "")) {
+                                        that.getView().byId("addWorkType").setSelectedKey("2");
+                                        that.getView().byId("workSection").setVisible(true);
+                                    }
+                                    if ((oData.results[0].OtherAddressLine1 !== "") || (oData.results[0].OtherAddressLine2 !== "")) {
+                                        that.getView().byId("addOtherType").setSelectedKey("3");
+                                        that.getView().byId("otherSection").setVisible(true);
+                                    }
+                                    that.getView().getModel("custModel").refresh();
+
+                                }
+                                else {
+                                    sap.m.MessageBox.show("Customer does not exist. Kindly add it", {
+                                        icon: sap.m.MessageBox.Icon.Error,
+                                        title: "Error",
+                                        actions: [MessageBox.Action.OK],
+                                        onClose: function (oAction) {
+                                            if (oAction === MessageBox.Action.OK) {
+                                                that.getView().getModel("custModel").setData({});
+                                            }
+                                        }
+                                    });
                                 }
                             }
-                        });
-                    }
-                });
+
+                        },
+                        error: function (oError) {
+                            sap.m.MessageBox.show("Customer does not exist. Kindly add it", {
+                                icon: sap.m.MessageBox.Icon.Error,
+                                title: "Error",
+                                actions: [MessageBox.Action.OK],
+                                onClose: function (oAction) {
+                                    if (oAction === MessageBox.Action.OK) {
+                                        that.getView().getModel("custModel").setData({});
+                                    }
+                                }
+                            });
+                        }
+                    });
             },
-            onSave: function(oEvent){
-                var that = this;
-                var data = this.getView().getModel("custModel").getData();
-                data.add_type="";
-                this.oModel.create("/ZER_CUST_MASTERSet", data, {
-                    success: function (oData) {
-                        that.getView().getModel("custModel").setData({});
-                        that.getView().byId("_IDGenSearchField").setValue("");
-                        sap.m.MessageToast.show("Entry Created Successfully");
-                    },
-                    error: function (oError) {
-                        sap.m.MessageToast.show("Error Creating Entry");
-                        
-                    }
-                });
+            convertToYYYYMMDD: function(dateStr) {
+                // Check if already in yyyyMMdd format
+                if(/^\d{ 8}$ /.test(dateStr)) {
+            return dateStr;
+        }
+
+        // Check if in dd/MM/yyyy format
+        const match = dateStr.match(/^(\d{2})\/(\d{2})\/(\d{4})$/);
+        if (match) {
+            const [, day, month, year] = match;
+            return `${year}${month}${day}`;
+        }
+
+        // Return original if format doesn't match
+        return dateStr;
+    },
+    onSave: function (oEvent) {
+        var that = this;
+        var data = this.getView().getModel("custModel").getData();
+        var birthDate = this.getView().byId("birthDate").getValue();
+        var expiryDate = this.getView().byId("expiryDate").getValue();
+
+        if (birthDate) {
+            data.BirthDate =new Date(birthDate);
+        }
+        else {
+            data.BirthDate = null;
+        }
+        if (expiryDate) {
+            data.IdentityExpiry = new Date(expiryDate);
+        }
+        else {
+            data.IdentityExpiry = null;
+        }
+        var bflag = false;
+        if (data.CustomerType === "TOURIST") {
+            if (data.IdentityExpiry && data.IdentityIssuedBy && data.IdentityIssuedBy && data.IdentityType) {
+                bflag = true;
+                this.getView().byId("cardType").setValueState("None");
+                this.getView().byId("issuedBy").setValueState("None");
+                this.getView().byId("cardNumber").setValueState("None");
+                this.getView().byId("expiryDate").setValueState("None");
             }
+            else {
+                bflag = false;
+                this.getView().byId("cardType").setValueState("Error");
+                this.getView().byId("issuedBy").setValueState("Error");
+                this.getView().byId("cardNumber").setValueState("Error");
+                this.getView().byId("expiryDate").setValueState("Error");
+
+            }
+        }
+        else {
+            bflag = true;
+        }
+        if (bflag) {
+            this.oModel.create("/CustomerSet", data, {
+                success: function (oData) {
+                    that.getView().getModel("custModel").setData({});
+                    that.getView().byId("_IDGenSearchField").setValue("");
+                    sap.m.MessageToast.show("Entry Created Successfully");
+                },
+                error: function (oError) {
+                    sap.m.MessageToast.show("Error Creating Entry");
+
+                }
+            });
+        }
+        else {
+            sap.m.MessageBox.error("Kindly fill the the required fields");
+        }
+    }
         });
     });
